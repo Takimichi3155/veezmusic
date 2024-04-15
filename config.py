27f3cbc9ba1909232a6390135c2f474e4526e2f8
Takimichi3155 <166791106+Takimichi3155@users.noreply.github.com>
2024-04-15 11:36:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,22 +7,22 @@ if os.path.exists("local.env"):
 
 load_dotenv()
 que = {}
-SESSION_NAME = getenv("SESSION_NAME", "session")
-BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME", "Veez Music")
+SESSION_NAME = getenv("SESSION_NAME", "Seista02)
+BOT_TOKEN = getenv("BOT_TOKEN", "6891505280:AAHC-b3eZuYgqw2ZgbWFC1afLYGPZ8bS9xY")
+BOT_NAME = getenv("BOT_NAME", "Seista")
 BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/d91623e9cefcef8fb3aa8.png")
 admins = {}
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-BOT_USERNAME = getenv("BOT_USERNAME", "veezmusicbot")
-ASSISTANT_NAME = getenv("ASSISTANT_NAME", "veezassistant")
-GROUP_SUPPORT = getenv("GROUP_SUPPORT", "gcsupportbots")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "levinachannel")
-OWNER_NAME = getenv("OWNER_NAME", "dlwrml") # isi dengan username kamu tanpa simbol @
-DEV_NAME = getenv("DEV_NAME", "dlwrml")
+API_ID = int(getenv("API_ID", "26977508"))
+API_HASH = getenv("API_HASH", "396589629e6705c592bc7fe891dc6e37")
+BOT_USERNAME = getenv("BOT_USERNAME", "SiestaProX_Robot")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "Seista02")
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "Dare_Devils_01")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "Seista_Updates")
+OWNER_NAME = getenv("OWNER_NAME", "aye_ujjwal") # isi dengan username kamu tanpa simbol @
+DEV_NAME = getenv("DEV_NAME", "aye_ujjwal")
 PMPERMIT = getenv("PMPERMIT", None)
 
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
 
